44fbafc66d81cd9bbd82e134f711d57fc0f8f36a
Jianhua Zhang <jzhang@jimmy.harvard.edu>
2007-03-29 15:19:18 +0000

    isGeneric removed throughout.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/cghMCR@23518 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/cghMCR.R
+++ R/cghMCR.R
@@ -14,66 +14,48 @@ setClass("cghMCR", representation(DNASeg = "data.frame",
                                   alteredHigh = "numeric",
                                   recurrence = "numeric"))
 
-if(!isGeneric("DNASeg")){
-  setGeneric("DNASeg",
-             function(object) standardGeneric("DNASeg"))
-}
+setGeneric("DNASeg",
+           function(object) standardGeneric("DNASeg"))
 setMethod("DNASeg", "cghMCR",
           function(object) object@DNASeg)
 
-if(!isGeneric("DNAData")){
-  setGeneric("DNAData",
+setGeneric("DNAData",
              function(object) standardGeneric("DNAData"))
-}
 setMethod("DNAData", "cghMCR",
           function(object) object@DNAData)
 
-if(!isGeneric("altered")){
-  setGeneric("altered",
-             function(object) standardGeneric("altered"))
-}
+setGeneric("altered",
+           function(object) standardGeneric("altered"))
 setMethod("altered", "cghMCR",
           function(object) object@altered)
 
-if(!isGeneric("gapAllowed")){
-  setGeneric("gapAllowed",
-             function(object) standardGeneric("gapAllowed"))
-}
+setGeneric("gapAllowed",
+           function(object) standardGeneric("gapAllowed"))
 setMethod("gapAllowed", "cghMCR",
           function(object) object@gapAllowed)
 
-if(!isGeneric("alteredLow")){
-  setGeneric("alteredLow",
-             function(object) standardGeneric("alteredLow"))
-}
+setGeneric("alteredLow",
+           function(object) standardGeneric("alteredLow"))
 setMethod("alteredLow", "cghMCR",
           function(object) object@alteredLow)
 
-if(!isGeneric("alteredHigh")){
-  setGeneric("alteredHigh",
-             function(object) standardGeneric("alteredHigh"))
-}
+setGeneric("alteredHigh",
+           function(object) standardGeneric("alteredHigh"))
 setMethod("alteredHigh", "cghMCR",
           function(object) object@alteredHigh)
 
-if(!isGeneric("spanLimit")){
-  setGeneric("spanLimit",
-             function(object) standardGeneric("spanLimit"))
-}
+setGeneric("spanLimit",
+           function(object) standardGeneric("spanLimit"))
 setMethod("spanLimit", "cghMCR",
           function(object) object@spanLimit)
 
-if(!isGeneric("recurrence")){
-  setGeneric("recurrence",
-             function(object) standardGeneric("recurrence"))
-}
+setGeneric("recurrence",
+           function(object) standardGeneric("recurrence"))
 setMethod("recurrence", "cghMCR",
           function(object) object@recurrence)
 
-if(!isGeneric("spans")){
-  setGeneric("spans",
-             function(object) standardGeneric("spans"))
-}
+setGeneric("spans",
+           function(object) standardGeneric("spans"))
 setMethod("spans", "cghMCR",
           function(object) object@spans)
   
@@ -100,10 +82,8 @@ setMethod("print", "cghMCR",
             }
           })
 
-if(!isGeneric("MCR")){
-  setGeneric("MCR",
-             function(object) standardGeneric("MCR"))
-}
+setGeneric("MCR",
+           function(object) standardGeneric("MCR"))
 setMethod("MCR", "cghMCR",
           function(object) {
             temp <- getMCR(object)
@@ -112,75 +92,55 @@ setMethod("MCR", "cghMCR",
           })
 
 ####### Replace methods
-if(!isGeneric("DNASeg<-")){
-  setGeneric("DNASeg<-", function(object, value)
-             standardGeneric("DNASeg<-"))
-}
+setGeneric("DNASeg<-", function(object, value)
+           standardGeneric("DNASeg<-"))
 setReplaceMethod("DNASeg", "cghMCR", function(object, value){
   object@DNASeg <- value; object})
 
-if(!isGeneric("DNAData<-")){
-  setGeneric("DNAData<-", function(object, value)
-             standardGeneric("DNAData<-"))
-}
+setGeneric("DNAData<-", function(object, value)
+           standardGeneric("DNAData<-"))
 setReplaceMethod("DNAData", "cghMCR", function(object, value){
   object@DNAData <- value; object})
 
-if(!isGeneric("altered<-")){
-  setGeneric("altered<-", function(object, value)
-             standardGeneric("altered<-"))
-}
+setGeneric("altered<-", function(object, value)
+           standardGeneric("altered<-"))
 setReplaceMethod("altered", "cghMCR", function(object, value){
   object@altered <- value; object})
 
-if(!isGeneric("gapAllowed<-")){
-  setGeneric("gapAllowed<-", function(object, value)
-             standardGeneric("gapAllowed<-"))
-}
+setGeneric("gapAllowed<-", function(object, value)
+           standardGeneric("gapAllowed<-"))
 setReplaceMethod("gapAllowed", "cghMCR", function(object, value){
   object@gapAllowed <- value; object})
 
-if(!isGeneric("alteredLow<-")){
-  setGeneric("alteredLow<-", function(object, value)
-             standardGeneric("alteredLow<-"))
-}
+setGeneric("alteredLow<-", function(object, value)
+           standardGeneric("alteredLow<-"))
 setReplaceMethod("alteredLow", "cghMCR", function(object, value){
   object@alteredLow <- value; object})
 
-if(!isGeneric("alteredHigh<-")){
-  setGeneric("alteredHigh<-", function(object, value)
-             standardGeneric("alteredHigh<-"))
-}
+setGeneric("alteredHigh<-", function(object, value)
+           standardGeneric("alteredHigh<-"))
 setReplaceMethod("alteredHigh", "cghMCR", function(object, value){
   object@alteredHigh <- value; object})
 
-if(!isGeneric("recurrence<-")){
-  setGeneric("recurrence<-", function(object, value)
-             standardGeneric("recurrence<-"))
-}
+setGeneric("recurrence<-", function(object, value)
+           standardGeneric("recurrence<-"))
 setReplaceMethod("recurrence", "cghMCR", function(object, value){
   object@recurrence <- value; object})
 
-if(!isGeneric("spanLimit<-")){
-  setGeneric("spanLimit<-", function(object, value)
-             standardGeneric("spanLimit<-"))
-}
+setGeneric("spanLimit<-", function(object, value)
+           standardGeneric("spanLimit<-"))
 setReplaceMethod("spanLimit", "cghMCR", function(object, value){
   object@spanLimit <- value; object})
 
-if(!isGeneric("spans<-")){
-  setGeneric("spans<-", function(object, value)
-             standardGeneric("spans<-"))
-}
+setGeneric("spans<-", function(object, value)
+           standardGeneric("spans<-"))
 setReplaceMethod("spans", "cghMCR", function(object, value){
   object@spans <- value; object})
 
 
 # A method for segmenting arrayCGH data added for marrayRaw
-if(!isGeneric("getSegments")){
-  setGeneric("getSegments",
-             function(object) standardGeneric("getSegments"))
-}
+setGeneric("getSegments",
+           function(object) standardGeneric("getSegments"))
 setMethod("getSegments", "marrayRaw",
           function(object) getSegData(object))
 
